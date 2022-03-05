ab1a75f998a6f7106a6c227c0edac4cab70f96ea
svaldessamperio <svaldes_samperio@hotmail.com>
2022-03-05 16:04:57 -0600

    Objeto notificacion como parte de la petición

--- src/routes/Notify.js
+++ src/routes/Notify.js
@@ -5,19 +5,14 @@ const conf = require('../../conf.json');
 const sql = require('mssql');
 const { messaging } = require('firebase-admin');
 
-async function sendMessageToTokens (tokens){
+async function sendMessageToTokens (tokens, notification){
     if (tokens.length>0) {
         // var token = ["fOA6tGwcSfePTw2GN5oHLN:APA91bGVpPBcuFmRKBzvcfjVfG6cZKxgZ-cBzwH_uw_ZupmN_DaHRWDAKl4p507My8ofB3bUDcgFydEzfzXQw7wLPI1HcLYQ-qNRT23ijMIsgk02RaiaulbcoNRSdaQS1X_SUV1UN1Wh", 
         //             "fX3DCnp0TKyghzHWxYnkGY:APA91bFwjk90nIO_CZoVfMknGnpsquoGtOiT3ye5ZEJSc6HJsI_fZ53FGk4BpdUq5Va5WN0TiWbxlF7MXv79FznqOIStre92ioTZjazWiYvOdGNL1RASleF-GW-GQ6IY5ZlipH7iQsWn",
         //             "dED4PMhV_UMxuOD5D_-eEj:APA91bEB1HSs9zT88ub87H7d8Fzsd_MTe-eYW0y7egTQRKc0COv_szEvh-7LUjD2Me1vJB015oc4GEZ-NayAMizcvxLryy1346eElrdXrPmYd3rONtSYzn1jyC04xc5FulxesDIhgWF8"
         //             ];
         var payload = {
-            notification: {
-                title:'Mi titulo',
-                sound: 'Pizzicato.wav',
-                body: 'El cuerpo',
-                imageUrl: 'https://www.grupoandrade.com/CommonUtilities/Images/Header/Logo-Andrade.png',
-            },
+            notification,
             data: {
                 MyKey1: "Hello"
             }
@@ -41,7 +36,7 @@ async function sendMessageToTokens (tokens){
 
 router.post('/sendMessageByEmail', (req, res) => {
 
-    const { email, tokenObject } = req.body;
+    const { email, notification } = req.body;
     const tokens = [];
 
     sql.connect(conf).then(pool => {
@@ -57,7 +52,7 @@ router.post('/sendMessageByEmail', (req, res) => {
             }
         }
 
-        const send = sendMessageToTokens(tokens);
+        const send = sendMessageToTokens(tokens, notification);
 
     }).catch(err => {
         console.log('SEGUNDO ERR: ' + err);
